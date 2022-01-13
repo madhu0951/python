ef3933a9426bf36eb1ab096f1edac583bd954caa
vanganur madhusudhan <89246442+madhu0951@users.noreply.github.com>
2022-01-13 13:43:39 +0530

    Update single inheritance.py

--- OOPs concepts/Inheritance/single inheritance.py
+++ OOPs concepts/Inheritance/single inheritance.py
@@ -1,1 +1,20 @@
+class parent:
+    def __init__(self,fname,age):
+        self.fname=fname
+        self.age=age
+    
+    def view(self):
+        print(self.fname,self.age)
 
+
+class child(parent):
+    def __init__(self,fname,age):
+        parent.__init__(self,fname,age)
+        self.lastname='reddy'
+    
+    def view(self):
+        print(self.age,self.fname,self.lastname)
+
+
+ob=child('madhusudhan',23)
+ob.view()

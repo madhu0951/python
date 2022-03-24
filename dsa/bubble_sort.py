87888db064ed18837fbe625ae0c3023f6e410eec
vanganur madhusudhan <89246442+madhu0951@users.noreply.github.com>
2022-03-24 11:24:50 +0530

    Update bubble_sort.py

--- dsa/bubble_sort.py
+++ dsa/bubble_sort.py
@@ -9,3 +9,25 @@ def bubble_sort(elements):
 elements=[5,9,2,1,67,34,88,34]
 bubble_sort(elements)
 print(elements)
+
+
+
+
+def bubbleSort(arr):
+    flag=False
+    for i in range(0,len(arr)-1):
+        for j in range(len(arr)-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                flag=True
+        if flag:
+            flag=False
+        else:
+            break
+    return arr
+print(bubbleSort([67,8,34,23,54,65,75,23,4,45]))
+
+#Non recursive
+#Stable
+#In place
+#O(n²)
